8e0fc08960f004a502324673456ad7b2d2ca6a8b
SayanDutta001 <84002255+SayanDutta001@users.noreply.github.com>
2021-08-10 12:28:37 +0530

    Create chocolateDistributionProblem.cpp

---
+++ chocolateDistributionProblem.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define mt              make_tuple
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> 
+#define setbits(x)      __builtin_popcountll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define range(a,b)		substr(a,b-a+1)
+#define w(x)            int x; cin>>x; while(x--)
+#define trace(x) 		cerr<<#x<<": "<<x<<" "<<endl;
+#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int chocolateDistribution(vi&arr,int m)
+{
+    int n = arr.size();
+    if(m>n)
+        return -1;
+    sort(arr.begin(),arr.end());
+    int i,j;
+    i = 0, j=m-1;
+    int mi = INT_MAX;
+    while(j<n)
+    {
+        mi = min(mi,arr[j] - arr[i]);
+        i++,j++;
+    }
+    return mi;
+}
+
+int32_t main()
+{
+	FIO;
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+    #endif
+	
+	int n,k;
+    cin>>n>>k;
+    vi arr;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    cout<<chocolateDistribution(arr,k);
+	return 0;
+}
